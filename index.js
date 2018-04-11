556c4f2bad079f5f09d849bdb348bddcf72c02d5
Gabriel Raybould <graybould@rebarsys.com>
2018-04-11 09:50:16 -0400

    Modified Jest-Junit-Reporter to output test results in a structured JSON file

--- index.js
+++ index.js
@@ -1,13 +1,56 @@
 const Testsuites = require('./src/Testsuites');
+const Failure = require('./src/Failure');
 const xml = require('xml');
 const fs = require('fs');
 const path = require('path');
 
+
 module.exports = (results) => {
-  const out = process.env.TEST_REPORT_PATH || process.cwd();
-  const filename = process.env.TEST_REPORT_FILENAME || 'test-report.xml';
-  const testSuites = new Testsuites(results);
-  const data = xml(testSuites, { declaration: true, indent: '  ' });
-  fs.writeFileSync(path.join(out, filename), data);
-  return results;
+
+    const data =
+        {
+            numFailedTestSuites: results.numFailedTestSuites,
+            numFailedTests: results.numFailedTests,
+            numPassedTestSuites: results.numPassedTestSuites,
+            numPassedTests: results.numPassedTests,
+            numPendingTestSuites: results.numPendingTestSuites,
+            numPendingTests: results.numPendingTests,
+            numRuntimeErrorTestSuites: results.numRuntimeErrorTestSuites,
+            numTotalTestSuites: results.numTotalTestSuites,
+            numTotalTests: results.numTotalTests,
+            startTime: results.startTime,
+            success: results.success,
+            testResults: []
+        };
+
+    data.testResults =
+        results.testResults.map((testSuite)=>{
+            return {
+                filePath : testSuite.testFilePath,
+                fileName : testSuite.testFilePath.substring(testSuite.testFilePath.lastIndexOf("\\") + 1),
+                fileDirectory : testSuite.testFilePath.substring(testSuite.testFilePath.indexOf("integration")+12,testSuite.testFilePath.lastIndexOf("\\")),
+                passed : testSuite.numPassedTests,
+                failed : testSuite.numFailingTests,
+                time : (testSuite.perfStats.end - testSuite.perfStats.start) / 1000,
+                timeStamp : new Date(testSuite.perfStats.start).toISOString().slice(0, -5),
+                testCases : testSuite.testResults.map((testCase)=>{
+
+                    return {
+                        sequenceNumber: testSuite.testResults.indexOf(testCase),
+                        ancestors: testCase.ancestorTitles,
+                        failureMessages : testCase.failureMessages.map(message => new Failure(message)),
+                        fullName : testCase.fullName,
+                        status : testCase.status,
+                        testCase : testCase.title
+                    };
+                })
+            };
+        });
+
+    const out = process.env.TEST_REPORT_PATH || process.cwd();
+    const filename = process.env.TEST_REPORT_FILENAME || 'test-report.json';
+
+    fs.writeFileSync(path.join(out, filename), JSON.stringify(data, null,"\t"));
+
+    return results;
 };
--- src/Failure.js
+++ src/Failure.js
@@ -1,12 +1,38 @@
 class Failure {
   constructor (message) {
-    this.failure = [{
-      _attr: {
-        message,
-        type: 'AssertionError'
+
+    var fix = message.replace(/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g, '');
+    fix = fix.replace(/(?:\r\n|\r|\n|\t)/g, ' ');
+
+    var startResult = 0;
+    var endResult = 0;
+
+    if (fix.indexOf("Expected value to equal") >= 0){
+      startResult = fix.indexOf("Expected value");
+      endResult = fix.indexOf("Difference:");
+    }else{
+        startResult = fix.indexOf("Expected value");
+        endResult = fix.indexOf("at Object.<anon");
+    }
+
+    message = fix.substring(startResult, endResult);
+
+    var expect = message.substring(0, message.indexOf("Received:"));
+    var receive = message.substring(message.indexOf("Received:"));
+
+    expect = expect.replace(/'/g, '\"');
+    receive = receive.replace(/'/g, '\"');
+
+    console.log(expect);
+    console.log(receive);
+
+      this.failure =
+      {
+        expected: expect,
+        received: receive
       }
-    }];
   }
 }
 
 module.exports = Failure;
+
